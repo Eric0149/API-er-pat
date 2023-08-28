98d30a3a18dc91fdc7fe1c0fdfe573c92f4ff3f7
eric0149 <eriscorplock@gmail.com>
2023-08-28 09:38:21 +0200

    some frontend issues upon deployment

--- gura/apiAPP/urls.py
+++ gura/apiAPP/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import ProductListView, CartView, OrderListView, OrderCreateAPIView, AddToCartView
 from drf_yasg.views import get_schema_view
 # for Documenting orders
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls.static import static
 from gura import settings
